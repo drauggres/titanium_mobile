8f6675a2ebe8ee2c70e8e3225901575f2549e9e2
Sergey Volkov <s.volkov@netris.ru>
2018-01-11 15:30:39 +0300

    [TIMOB-25663] Android. Add HttpClient.cache property

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -1091,6 +1091,11 @@ public class TiC
 	/**
 	 * @module.api
 	 */
+	public static final String PROPERTY_CACHE = "cache";
+
+	/**
+	 * @module.api
+	 */
 	public static final String PROPERTY_CACHE_MODE = "cacheMode";
 
 	/**
