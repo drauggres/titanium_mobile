a54a4e9c437c76109494767d5de40831dbe25aca
Emin A. Alekperov <e.alekperov@netris.ru>
2020-05-21 06:07:37 +0300

    feat: add a "playbackRate" property to VideoPlayer API
    By this feature you can change playback speed

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -3572,6 +3572,11 @@ public class TiC
 	/**
 	 * @module.api
 	 */
+	public static final String PROPERTY_PLAYBACK_RATE = "playbackRate";
+
+	/**
+	 * @module.api
+	 */
 	public static final String PROPERTY_WAKE_LOCK = "wakeLock";
 
 	/**
