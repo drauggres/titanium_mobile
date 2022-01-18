a58ebb4cca280145abb3c83459eeefed97d78a8d
Sergey Volkov <s.volkov@netris.ru>
2022-01-18 17:02:15 +0300

    fix(android): separator in ListView with separatorStyle "TABLE_VIEW_SEPARATOR_STYLE_NONE"

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIListView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIListView.java
@@ -215,7 +215,10 @@ public class TiUIListView extends TiUIView
 				? TiConvert.toTiDimension(heightString, TiDimension.TYPE_HEIGHT)
 				.getAsPixels((View) getNativeView().getParent()) : 0;
 
-			if (name.equals(TiC.PROPERTY_SEPARATOR_COLOR)
+			// UI.TABLE_VIEW_SEPARATOR_STYLE_NONE always has zero height
+			if (height == 0) {
+				this.listView.setSeparator(0, height);
+			} else if (name.equals(TiC.PROPERTY_SEPARATOR_COLOR)
 				|| properties.containsKey(TiC.PROPERTY_SEPARATOR_COLOR)) {
 				String colorString = properties.getString(TiC.PROPERTY_SEPARATOR_COLOR);
 				if (name.equals(TiC.PROPERTY_SEPARATOR_COLOR)) {
